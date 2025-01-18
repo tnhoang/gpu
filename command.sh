93607a0ceeb9ad551768c3ba07580a89d0860306
tnhoang <tnhoang088@gmail.com>
2025-01-18 22:56:05 +0700

    update command

--- command.sh
+++ command.sh
@@ -1,4 +1,6 @@
 sudo apt update -y
-sudo apt install nvtop
-sudo apt install ffmpeg
+sudo apt install nvtop vim nvim ffmpeg -y
 pip install whisperx
+
+sudo apt install nvidia-cudnn
+sudo apt-get install --reinstall libcudnn8 libcudnn8-dev libcudnn8-samples
